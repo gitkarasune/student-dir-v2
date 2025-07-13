70b09c4a7e62840a64f80b224e125f72dbeb25a4
unknown <sunebarikara@gmail.com>
2025-07-13 04:00:16 +0100

    changes on next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,18 @@ const nextConfig: NextConfig = {
       // add other domains if needed
     ],
   },
+
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'], 
+  eslint: {
+     // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true, 
+  },
+  typescript: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has TypeScript errors.
+    ignoreBuildErrors: true, 
+  },
 };
 
 export default nextConfig;
